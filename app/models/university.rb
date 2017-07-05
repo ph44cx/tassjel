b71cc7dd72fa37edf3c7ef4fd69cf49cae93cf30
ANIL VERMA <aneilwebber@gmail.com>
2017-07-05 15:30:47 +0530

    faculty

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,5 +2,6 @@ class University < ApplicationRecord
 	belongs_to :university_profile
 	has_many :faculties, :dependent => :destroy
 	COUNTRY= ['Turkey', 'Ukraine', 'Malaysia', 'Cyprus', 'Canada', 'Russia', 'UK', 'Germany']
+	FACULTYSPECIALITY= ['Bio-technolgy', 'Software', 'Mathematics', 'Science', 'Artifical Intelligence', 'Robotics']
 	accepts_nested_attributes_for :faculties, allow_destroy: true
 end
